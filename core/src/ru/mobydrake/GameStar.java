469621c6e785dfb4dbb9800f1d5ecb37e4ef081b
MobyDrake <milkky598@yandex.ru>
2019-07-15 12:16:05 +0500

    lesson2

--- core/src/ru/mobydrake/GameStar.java
+++ core/src/ru/mobydrake/GameStar.java
@@ -1,35 +1,18 @@
 package ru.mobydrake;
 
 import com.badlogic.gdx.ApplicationAdapter;
+import com.badlogic.gdx.Game;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 
-public class GameStar extends ApplicationAdapter {
-	SpriteBatch batch;
-	Texture img;
-	
-	@Override
-	public void create () {
-		batch = new SpriteBatch();
-		//img = new Texture("badlogic.jpg");
-		img = new Texture("1235845.jpg");
-	}
+import ru.mobydrake.screens.MenuScreen;
 
-	@Override
-	public void render () {
-		Gdx.gl.glClearColor(1, 0, 0, 1);
-		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
-		batch.begin();
-		//batch.draw(img, 0, 0);
-		batch.draw(img, 0, 0, Gdx.graphics.getWidth(), Gdx.graphics.getHeight());
-		batch.end();
-	}
-	
-	@Override
-	public void dispose () {
-		batch.dispose();
-		img.dispose();
-	}
+public class GameStar extends Game  {
+
+    @Override
+    public void create() {
+        setScreen(new MenuScreen());
+    }
 }
--- core/src/ru/mobydrake/screens/MenuScreen.java
+++ core/src/ru/mobydrake/screens/MenuScreen.java
@@ -1,4 +1,97 @@
 package ru.mobydrake.screens;
 
-public class MenuScreen {
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Input;
+import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.math.Vector2;
+
+import ru.mobydrake.base.BaseScreen;
+
+public class MenuScreen extends BaseScreen {
+
+    private Texture texture;
+    private Vector2 img;
+    private Vector2 speedV;
+    private Vector2 touch;
+    private boolean moveByMouse = true;
+    private int speedMouse = 2;
+
+
+    @Override
+    public void show() {
+        super.show();
+        texture = new Texture("463712.png");
+        img = new Vector2(0, 0);
+        touch = new Vector2();
+    }
+
+    @Override
+    public void render(float delta) {
+        super.render(delta);
+
+        moveToPointMouse();
+        moveToArrows();
+
+        Gdx.gl.glClearColor(0.26f, 0.5f, 0.8f, 1);
+        Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+        batch.begin();
+        batch.draw(texture, img.x, img.y);
+        batch.end();
+    }
+
+    @Override
+    public void dispose() {
+        super.dispose();
+        texture.dispose();
+    }
+
+    @Override
+    public boolean touchDown(int screenX, int screenY, int pointer, int button) {
+        touch.set(screenX, Gdx.graphics.getHeight() - screenY);
+        return false;
+    }
+
+    @Override
+    public boolean keyTyped(char character) {
+        if (character == 'q') {
+            System.out.println("touch: " + touch.x + " " + touch.y);
+            System.out.println("img: " + img.x + " " + img.y);
+            System.out.println("speed:" + speedV.x + " " + speedV.y);
+        }
+
+        //переключение режима движуния
+        if (character == 'n') {
+            moveByMouse = !moveByMouse;
+        }
+
+        return false;
+    }
+
+    private void moveToPointMouse() {
+        if (moveByMouse) {
+            if (img.x != touch.x || img.y != touch.y) {
+                if (img.x < touch.x) {
+                    img.add(speedMouse, 0);
+                }
+                if (img.x > touch.x) {
+                    img.add(-speedMouse, 0);
+                }
+                if (img.y < touch.y) {
+                    img.add(0, speedMouse);
+                }
+                if (img.y > touch.y) {
+                    img.add(0, -speedMouse);
+                }
+            }
+        }
+    }
+
+    private void moveToArrows() {
+        if (Gdx.input.isKeyPressed(Input.Keys.RIGHT)) img.add(4, 0);
+        if (Gdx.input.isKeyPressed(Input.Keys.LEFT)) img.add(-4, 0);
+        if (Gdx.input.isKeyPressed(Input.Keys.UP)) img.add(0, 4);
+        if (Gdx.input.isKeyPressed(Input.Keys.DOWN)) img.add(0, -4);
+    }
+
 }
