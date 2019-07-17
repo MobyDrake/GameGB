f8cd584aa9ac7f4916b4acefbfce5930d0a04ed9
MobyDrake <milkky598@yandex.ru>
2019-07-17 14:46:38 +0500

    Передвижение картинке к курсору и движение при помощи мышки

--- core/src/ru/mobydrake/screens/MenuScreen.java
+++ core/src/ru/mobydrake/screens/MenuScreen.java
@@ -11,19 +11,23 @@ import ru.mobydrake.base.BaseScreen;
 public class MenuScreen extends BaseScreen {
 
     private Texture texture;
-    private Vector2 img;
+
+    private Vector2 imgPosition;
     private Vector2 speedV;
     private Vector2 touch;
-    private boolean moveByMouse = true;
-    private int speedMouse = 2;
+    private Vector2 touchBuff;
+
+    private final float V_LEN = 2f;
 
 
     @Override
     public void show() {
         super.show();
         texture = new Texture("463712.png");
-        img = new Vector2(0, 0);
+        imgPosition = new Vector2(0, 0);
         touch = new Vector2();
+        touchBuff = new Vector2();
+        speedV = new Vector2();
     }
 
     @Override
@@ -36,7 +40,7 @@ public class MenuScreen extends BaseScreen {
         Gdx.gl.glClearColor(0.26f, 0.5f, 0.8f, 1);
         Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
         batch.begin();
-        batch.draw(texture, img.x, img.y);
+        batch.draw(texture, imgPosition.x, imgPosition.y);
         batch.end();
     }
 
@@ -49,49 +53,38 @@ public class MenuScreen extends BaseScreen {
     @Override
     public boolean touchDown(int screenX, int screenY, int pointer, int button) {
         touch.set(screenX, Gdx.graphics.getHeight() - screenY);
+        speedV.set(touch.cpy().sub(imgPosition)).setLength(V_LEN);
         return false;
     }
 
     @Override
     public boolean keyTyped(char character) {
+
         if (character == 'q') {
             System.out.println("touch: " + touch.x + " " + touch.y);
-            System.out.println("img: " + img.x + " " + img.y);
+            System.out.println("imgPosition position: " + imgPosition.x + " " + imgPosition.y);
             System.out.println("speed:" + speedV.x + " " + speedV.y);
-        }
-
-        //переключение режима движуния
-        if (character == 'n') {
-            moveByMouse = !moveByMouse;
         }
 
         return false;
     }
 
     private void moveToPointMouse() {
-        if (moveByMouse) {
-            if (img.x != touch.x || img.y != touch.y) {
-                if (img.x < touch.x) {
-                    img.add(speedMouse, 0);
-                }
-                if (img.x > touch.x) {
-                    img.add(-speedMouse, 0);
-                }
-                if (img.y < touch.y) {
-                    img.add(0, speedMouse);
-                }
-                if (img.y > touch.y) {
-                    img.add(0, -speedMouse);
-                }
-            }
+
+        touchBuff.set(touch);
+
+        if(touchBuff.sub(imgPosition).len() > V_LEN) {
+            imgPosition.add(speedV);
+        } else {
+            imgPosition.set(touch);
         }
     }
 
     private void moveToArrows() {
-        if (Gdx.input.isKeyPressed(Input.Keys.RIGHT)) img.add(4, 0);
-        if (Gdx.input.isKeyPressed(Input.Keys.LEFT)) img.add(-4, 0);
-        if (Gdx.input.isKeyPressed(Input.Keys.UP)) img.add(0, 4);
-        if (Gdx.input.isKeyPressed(Input.Keys.DOWN)) img.add(0, -4);
+        if (Gdx.input.isKeyPressed(Input.Keys.RIGHT)) imgPosition.add(4, 0);
+        if (Gdx.input.isKeyPressed(Input.Keys.LEFT)) imgPosition.add(-4, 0);
+        if (Gdx.input.isKeyPressed(Input.Keys.UP)) imgPosition.add(0, 4);
+        if (Gdx.input.isKeyPressed(Input.Keys.DOWN)) imgPosition.add(0, -4);
     }
 
 }
